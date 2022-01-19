b1e77afd280ab659271d299095eb20e2d0f2d203
Alex Guizar <alexguizar90@gmail.com>
2022-01-18 20:50:09 -0800

    Added the ability to add a movie to the favorites list

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -8,17 +8,27 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 })
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
+  currentUser: any = '';
   constructor(public fetchApiData: FetchApiDataService) { }
 
   ngOnInit(): void {
+    this.currentUser = localStorage.getItem('user');
     this.getMovies();
   }
 
   getMovies(): void {
-    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-      this.movies = resp;
+    this.fetchApiData.getAllMovies().subscribe((result: any) => {
+      this.movies = result;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  addFavorite(movieId: any): void {
+    this.fetchApiData.addUserFavorite(this.currentUser, movieId).subscribe((result: any) => {
+      // Add color change to favorite icon
+      console.log(result);
+      localStorage.setItem('favorites', JSON.stringify(result.FavoriteMovies));
     });
   }
 }
