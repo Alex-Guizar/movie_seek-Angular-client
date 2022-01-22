37d1300190e437b143851d3bb8d628cb95f4267e
Alex Guizar <alexguizar90@gmail.com>
2022-01-21 18:03:18 -0800

    Added logic to determine the contents of toolbar based on login status. Added styling for toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie_seek-Angular-client';
+  loggedIn = (localStorage.getItem('token') !== null ? true: false);
 
-  constructor() { }
+  constructor(
+    public router: Router
+  ) { }
 
-  
+  /**
+   * Detect whether a token exists in localStorage
+   * If not, set loggedIn to false and provide reduced toolbar
+   */
+  ngOnInit(): void {
+    this.router.events.subscribe(event =>{
+      if (event instanceof NavigationStart){
+         this.loggedIn = (localStorage.getItem('token') !== null ? true: false);
+      }
+    });
+  }
+
+  /**
+   * Toolbar logo navigation to 'movies'
+   */
+  goToMovies(): void {
+    this.router.navigate(['movies']);
+  }
+
+  /**
+   * Toolbar profile navigation to 'profile'
+   */
+  goToProfile(): void {
+    this.router.navigate(['profile']);
+  }
+
+  /**
+   * Toolbar logout. Removes all localStorage items
+   * and navigates to 'welcome'
+   */
+  logOut(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    localStorage.removeItem('favorites');
+    this.loggedIn = false;
+    this.router.navigate(['welcome']);
+  }
 }
